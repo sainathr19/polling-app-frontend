007abcbeea0abf50ffec1ebce7dd6ab78021246d
Sainath Reddy <gsnr1925@gmail.com>
2024-11-19 18:43:45 +0530

    log errors

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,63 +1,48 @@
-import toast from "react-hot-toast";
 import axiosInstance from "./api.service";
 
-async function startRegistration(username : string){
-    try{
-        console.log(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/auth/register/start/${username}`)
+async function startRegistrationWithUsername(username: string) {
+    try {
         const response = await axiosInstance.post(`/auth/register/start/${username}`);
-        return response.data;
-    }catch(err){
-        console.log(err);
+        return response.data.publicKey;
+    } catch (err) {
+        console.error(err);
+        throw new Error("Failed to retrieve registration options from backend");
     }
 }
 
+async function finishRegistrationwithUsername(username: string, credentials: any) {
+    try {
+        const response = await axiosInstance.post(`/auth/register/finish/${username}`, credentials);
+        return response.data;
+    } catch (err) {
+        console.error(err);
+        throw new Error("Failed to complete registration");
+    }
+}
 
 async function startAuthenticationWithUsername(username: string) {
     try {
-      const response = await axiosInstance.post(`/auth/authenticate/start/${username}`);
-      return response.data.publicKey;
+        const response = await axiosInstance.post(`/auth/authenticate/start/${username}`);
+        return response.data.publicKey;
     } catch (err) {
-      console.error(err);
-      throw new Error("Failed to retrieve registration options from backend");
+        console.error(err);
+        throw new Error("Failed to retrieve authentication options from backend");
     }
-  }
-
-async function finishAuthenticationwithUsername(username: string, credentials: any) {
-  try {
-    const response = await axiosInstance.post(`/auth/authenticate/finish/${username}`, credentials);
-    console.log(response);
-    if (response.status === 200) {
-      toast.success("SignIn successful!");
-    } else {
-      toast.error("SignIn failed, please try again.");
-    }
-  } catch (err) {
-    console.error(err);
-    throw new Error("Failed to retrieve Authentication options from backend");
-  }
 }
 
-  async function startRegistrationWithUsername(username: string) {
+async function finishAuthenticationwithUsername(username: string, credentials: any) {
     try {
-      const response = await axiosInstance.post(`/auth/register/start/${username}`);
-      return response.data.publicKey;
+        const response = await axiosInstance.post(`/auth/authenticate/finish/${username}`, credentials);
+        return response.data;
     } catch (err) {
-      console.error(err);
-      throw new Error("Failed to retrieve registration options from backend");
+        console.error(err);
+        throw new Error("Failed to complete authentication");
     }
-  }
+}
 
-  async function finishRegistrationwithUsername(username : string , credentials : any){
-    try {
-      const response = await axiosInstance.post(`/auth/register/finish/${username}`);
-      if (response.status === 200) {
-        toast.success("Registration successful!");
-      } else {
-        toast.error("Registration failed, please try again.");
-      }
-    } catch (err) {
-      console.error(err);
-      throw new Error("Failed to retrieve registration options from backend");
-    }
-  }
-export {startRegistrationWithUsername,finishRegistrationwithUsername, startAuthenticationWithUsername,finishAuthenticationwithUsername};
+export {
+    startRegistrationWithUsername,
+    finishRegistrationwithUsername,
+    startAuthenticationWithUsername,
+    finishAuthenticationwithUsername
+};
--- src/services/poll.service.ts
+++ src/services/poll.service.ts
@@ -1,106 +1,93 @@
-import axios, { AxiosError } from "axios";
 import axiosInstance from "./api.service";
-async function CreateNewPoll(username : string , title : string , options : string[]){
-    const reqBody = {
-        username,
-        title,
-        options
-    }
-    try{
-        const response = await axiosInstance.post(`/polls/new`,reqBody);
+
+async function CreateNewPoll(username: string, title: string, options: string[]){
+    try {
+        const reqBody = { username, title, options }
+        const response = await axiosInstance.post(`/polls/new`, reqBody);
         return response.data;
-    }catch(err){
-        console.log("Error Creating Poll : ",err);
+    } catch(err) {
+        console.log("Error Creating Poll : ", err);
         throw new Error("Error Creating New Poll");
     }
-} 
+}
 
-async function ResetPollById(pollId : string ){
-    const reqBody = {
-        username : "tklmro",
-        pollId
-    }
-    try{
-        const response = await axiosInstance.post(`/polls/${pollId}/reset`,reqBody);
-        return response.data;
-    }catch(err){
-        console.log("Error Creating Poll : ",err);
-        throw new Error("Error Creating New Poll");
-    }
-} 
-
-async function FetchWithPollId(pollId : string ){
-    try{
+async function FetchWithPollId(pollId: string){
+    try {
         const response = await axiosInstance.get(`/polls/${pollId}/search`);
         return response.data;
-    }catch(err){
-        console.log("Error Fetching Poll Data : ",err);
+    } catch(err) {
+        console.log("Error Fetching Poll Data : ", err);
         throw new Error("Error Fetching Poll Data");
     }
-} 
-
+}
 
 async function FetchAllPolls(){
-    try{
+    try {
         const response = await axiosInstance.get(`/all`);
         return response.data;
-    }catch(err){
-        console.log("Error Fetching Polls : ",err);
+    } catch(err) {
+        console.log("Error Fetching Polls : ", err);
         throw new Error("Error Fetching Polls");
     }
-} 
+}
 
-async function FetchUserPolls(userName : string){
-    try{
+async function FetchUserPolls(userName: string){
+    try {
         const response = await axiosInstance.get(`/all?userId=${userName}`);
         return response.data;
-    }catch(err){
-        console.log("Error Fetching Polls : ",err);
+    } catch(err) {
+        console.log("Error Fetching Polls : ", err);
         throw new Error("Error Fetching Polls");
     }
-} 
+}
 
-async function DeletPollById(pollId : string){
-    try{
+async function DeletPollById(pollId: string){
+    try {
         const response = await axiosInstance.get(`/polls/${pollId}/delete`);
         return response.data;
-    }catch(err){
-        console.log(`Error Deleting Poll with Id : ${pollId}`,err);
+    } catch(err) {
+        console.log(`Error Deleting Poll with Id : ${pollId}`, err);
         throw new Error("Error Deleting Poll");
     }
-} 
+}
 
-async function ResetPollByID(pollId : string){
-    try{
+async function ResetPollByID(pollId: string){
+    try {
         const response = await axiosInstance.get(`/polls/${pollId}/reset`);
         return response.data;
-    }catch(err){
-        console.log(`Error Resettign Poll with Id : ${pollId}`,err);
+    } catch(err) {
+        console.log(`Error Resetting Poll with Id : ${pollId}`, err);
         throw new Error("Error Resetting Poll");
     }
-} 
+}
 
-
-async function ClosePollByID(pollId : string){
-    try{
+async function ClosePollByID(pollId: string){
+    try {
         const response = await axiosInstance.post(`/polls/${pollId}/close`);
         return response.data;
-    }catch(err){
-        console.log(`Error Closing Poll with Id : ${pollId}`,err);
+    } catch(err) {
+        console.log(`Error Closing Poll with Id : ${pollId}`, err);
         throw new Error("Error Closing Poll");
     }
-} 
+}
 
-
-async function CastVote(pollId : string , optionId : string){
-    try{
+async function CastVote(pollId: string, optionId: string){
+    try {
         const response = await axiosInstance.post(`/polls/${pollId}/vote?option_id=${optionId}`);
         return response.data;
-    }catch(err){
-        let error : AxiosError = err as AxiosError;
-        console.log(`Error Voting for Poll with Id : ${pollId}`,err);
+    } catch(err) {
+        console.log(`Error Voting for Poll with Id : ${pollId}`, err);
         throw new Error("Error casting Vote");
     }
-} 
-export { CreateNewPoll, FetchWithPollId, FetchAllPolls, ResetPollByID,DeletPollById , ResetPollById , ClosePollByID,CastVote ,FetchUserPolls};
+}
 
+export { 
+    CreateNewPoll, 
+    FetchWithPollId, 
+    FetchAllPolls, 
+    ResetPollByID,
+    DeletPollById, 
+    ClosePollByID,
+    CastVote,
+    FetchUserPolls
+};
